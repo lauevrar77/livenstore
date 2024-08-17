b5a43894a53cfec1af27fabe17948d500a832914
laurent <evrardlaurent77@gmail.com>
2024-08-17 08:43:44 +0200

    check that event exists and is not already in stream when linking to stream

--- persistance/stream_writer.go
+++ persistance/stream_writer.go
@@ -1,6 +1,7 @@
 package persistance
 
 import (
+	"errors"
 	"fmt"
 	"os"
 
@@ -8,6 +9,8 @@ import (
 
 	"github.com/oklog/ulid/v2"
 )
+
+var AlreadyInStreamError = errors.New("event already in stream")
 
 type StreamWriter struct {
 	BasePath string
@@ -20,8 +23,22 @@ func NewStreamWriter(basePath string) StreamWriter {
 }
 
 func (sw *StreamWriter) LinkEvent(streamName string, eventID ulid.ULID) error {
-	//TODO : check that the event exists
-	//TODO : check that the event is not already in the stream
+	alreadyInStream, err := sw.alreadyInStream(streamName, eventID)
+	if err != nil {
+		return err
+	}
+	if alreadyInStream {
+		return AlreadyInStreamError
+	}
+
+	eventExists, err := sw.eventExists(eventID)
+	if err != nil {
+		return err
+	}
+	if !eventExists {
+		return EventNotFound
+	}
+
 	payload := writing.WriteStringBytes(eventID.String(), []byte{})
 	f, err := os.OpenFile(fmt.Sprintf("%s/%s.stream", sw.BasePath, streamName), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0777)
 	if err != nil {
@@ -35,4 +52,30 @@ func (sw *StreamWriter) LinkEvent(streamName string, eventID ulid.ULID) error {
 	}
 
 	return nil
+}
+
+func (sw *StreamWriter) eventExists(eventID ulid.ULID) (bool, error) {
+	reader := EventReader{BasePath: sw.BasePath}
+	_, err := reader.ReadEvent(eventID)
+	if err != nil {
+		if err == EventNotFound {
+			return false, nil
+		}
+		return false, err
+	}
+	return true, nil
+}
+
+func (sw *StreamWriter) alreadyInStream(streamName string, eventID ulid.ULID) (bool, error) {
+	reader := StreamReader{BasePath: sw.BasePath}
+	stream, err := reader.ReadStream(streamName)
+	if err != nil {
+		return false, err
+	}
+	for _, id := range stream.EventIDs {
+		if id == eventID {
+			return true, nil
+		}
+	}
+	return false, nil
 }
